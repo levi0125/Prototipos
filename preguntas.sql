4cc69150c9d8c29e61a960b20396ade74cc77a48
Martin <22301061550038@cetis155.edu.mx>
2024-08-07 13:06:49 -0600

    respuesta a las preguntas

--- preguntas.sql
+++ preguntas.sql
@@ -3,9 +3,15 @@ preguntas:
 
 
 -todos deben tener un nickName o nombre de cuenta? solo los alumnos?
+		tal vez si para todos
+        
 -guradamos las escuelas de tutores y alumnos? solo de uno o de ninguno?
+		solo tutor
+        
 -tanto tutor como alumno deben estar en alguna escuela? asi esten en el rango de edad si no tienen escuela no se pueden registrar?
--
+		solo tutores deben estar en una escuela si o si
+        
+-falta hacer el buzon
 -
 -
 
